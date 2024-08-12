ae4395bb50b9238e3e91eab9624a019c85fb9929
Pascal Billerach <p.billerach@gmail.com>
2024-08-12 19:04:31 +0200

    add before_action to companies ctrler

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,4 +1,5 @@
 class CompaniesController < ApplicationController
+  before_action :set_company, only: [:show,:edit,:update,:destroy]
 
   def index
     @companies = Company.all
@@ -16,23 +17,19 @@ class CompaniesController < ApplicationController
   end
 
   def show
-    @company = Company.find(params[:id])
     @performances = Performance.where(company: @company)
     @employee = Employee.new(company: @company)
     @performance = Performance.new(company: @company)
   end
 
   def edit
-    @company = Company.find(params[:id])
   end
 
   def update
-    @company = Company.find(params[:id])
     redirect_to @company, notice: 'Les données de la structure ont été mises à jour.' if @company.update(company_params)
   end
 
   def destroy
-    @company = Company.find(params[:id])
     @company.destroy
     redirect_to companies_path, notice: "#{@company.name} a été supprimé définitivement."
   end
@@ -41,5 +38,9 @@ class CompaniesController < ApplicationController
 
   def company_params
     params.require(:company).permit(:name, :siret, :naf, :address, :license_number)
+  end
+
+  def set_company
+    @company = Company.find(params[:id])
   end
 end
