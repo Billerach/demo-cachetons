df05f4cc8a95d09717a40b1eba8368c04a5f2274
Pascal Billerach <p.billerach@gmail.com>
2024-08-28 15:00:32 +0200

    seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,7 +31,7 @@ company1 = Company.create!(
   name: 'Les cantonniers de Corcové',
   siret: '123 568 941 00056',
   naf: '90.01Z',
-  address: '3 rue dugenou, 44156 Pâté-sur-mer',
+  address: '3 rue du gnou, 44156 Pâté-sur-mer',
   license_number: '1076958',
   specialty: 'troupe de théâtre'
 )
@@ -56,43 +56,43 @@ puts 'Companies created'
 
 puts 'Creatings performances'
 
-p1 = Performance.create!(company_id: company1.id, name: 'To be',             num_objet: '123Z68686634')
-p2 = Performance.create!(company_id: company2.id, name: 'La Déglingue',      num_objet: '321Z68686635')
-p3 = Performance.create!(company_id: company3.id, name: 'Mais oui mais non', num_objet: '741Z68686147')
+p1 = Performance.create!(company_id: company1.id, name: 'To be', num_objet: '123Z68686634')
+p2 = Performance.create!(company_id: company2.id, name: 'La Déglingue', num_objet: '321Z68686635')
+p3 = Performance.create!(company_id: company3.id, name: 'Oui mais non', num_objet: '741Z68686147')
 
-Performance.create!(company_id: company1.id, name: 'Jeux de Pouvoirs',               num_objet: '456Z68686661')
-Performance.create!(company_id: company1.id, name: 'Les Désossés',                   num_objet: '286Z68686628')
-Performance.create!(company_id: company1.id, name: 'Martine chez les bonnes soeurs', num_objet: '789Z68686613')
-Performance.create!(company_id: company1.id, name: 'Pli Pla Plou',                   num_objet: '147Z68686605')
-Performance.create!(company_id: company1.id, name: 'Partout',                        num_objet: '285Z68686601')
-Performance.create!(company_id: company2.id, name: 'Nulle Part Ici',                 num_objet: '369Z68686689')
-Performance.create!(company_id: company2.id, name: 'Duvel et des pelles',            num_objet: '654Z68686477')
-Performance.create!(company_id: company2.id, name: 'Paroles !',                      num_objet: '987Z68686111')
-Performance.create!(company_id: company3.id, name: "Sans dec'",                      num_objet: '159Z68686789')
-Performance.create!(company_id: company3.id, name: 'Le Grand Barzingue',             num_objet: '296Z68686456')
+Performance.create!(company_id: company1.id, name: 'Jeux de Pouvoirs', num_objet: '456Z68686661')
+Performance.create!(company_id: company1.id, name: 'Les Désossés', num_objet: '286Z68686628')
+Performance.create!(company_id: company1.id, name: 'Le silence des enfer', num_objet: '789Z68686613')
+Performance.create!(company_id: company1.id, name: 'Pli Pla Plou', num_objet: '147Z68686605')
+Performance.create!(company_id: company1.id, name: 'Cuillère', num_objet: '285Z68686601')
+Performance.create!(company_id: company2.id, name: 'Nulle Part', num_objet: '369Z68686689')
+Performance.create!(company_id: company2.id, name: "J'aurais voulu ne pas naître un artiste", num_objet: '654Z68686477')
+Performance.create!(company_id: company2.id, name: 'Affections Angulaires', num_objet: '987Z68686111')
+Performance.create!(company_id: company3.id, name: "Sans dec'", num_objet: '159Z68686789')
+Performance.create!(company_id: company3.id, name: 'Le Grand Barzingue', num_objet: '296Z68686456')
 
 puts 'Performances created'
 
 puts 'Creating employees'
 
-jean_mich = Employee.create!(
-  firstname: 'Jean-Mich',
-  lastname: 'Wallouhoualla',
+jean = Employee.create!(
+  firstname: 'Jean',
+  lastname: 'Calvin',
   company_id: company1.id,
   address: '142 rue du Cotentin, 44000 Nantes',
   phone: '0642457895',
   NIR: '185021708565428',
   conges_spectacles: '98746421',
   date_of_birth: Date.new(1981, 5, 28),
-  place_of_birth: 'Pétaouchnok',
-  departement_of_birth: '28',
+  place_of_birth: 'Vendôme',
+  departement_of_birth: '41',
   country_of_birth: 'France'
 )
 fabrice = Employee.create!(
   firstname: 'Fabrice',
   lastname: 'Le Meunier',
   company_id: company2.id,
-  address: "8 allée des grands sages, 29400 Ploumeuneuc'h",
+  address: "8 allée des grands sages, 22300 Ploubezre",
   phone: '0785458326',
   NIR: '011100110001111',
   conges_spectacles: '12345678',
@@ -105,7 +105,7 @@ mamadou = Employee.create!(
   firstname: 'Mamadou',
   lastname: 'Cissoko',
   company_id: company3.id,
-  address: "34 rue de l'Ouganda, 35000 Rennes",
+  address: "34 rue des Deux Chouettes, 35000 Rennes",
   phone: '0788556699',
   NIR: '336633663336633',
   conges_spectacles: '85285285',
@@ -114,22 +114,22 @@ mamadou = Employee.create!(
   departement_of_birth: '',
   country_of_birth: 'Mali'
 )
-luke = Employee.create!(
-  firstname: 'Luke',
-  lastname: 'Skywalker',
+luc = Employee.create!(
+  firstname: 'Luc',
+  lastname: 'Mosebacke',
   company_id: company1.id,
-  address: '32 passage du trou, 25060 Montcuq',
+  address: '32 passage de la coline, 25660 Montfaucon',
   phone: '0698798798',
   NIR: '555445554445544',
   conges_spectacles: '45645645',
   date_of_birth: Date.new(1962, 9, 12),
-  place_of_birth: 'Villedieu-Les-Poêles-sur-Saône',
-  departement_of_birth: '77',
+  place_of_birth: 'Villedieu',
+  departement_of_birth: '84',
   country_of_birth: 'France'
 )
 albert = Employee.create!(
   firstname: 'Albert',
-  lastname: 'de la Grenouillères des Andes',
+  lastname: 'Berthet',
   company_id: company2.id,
   address: '366 place des monuments aux vétérans, 55800 Argenton-le-Pont',
   phone: '0685789445',
@@ -137,33 +137,33 @@ albert = Employee.create!(
   conges_spectacles: '12312531',
   date_of_birth: Date.new(2000, 1, 2),
   place_of_birth: 'Châlon-sur-Saône',
-  departement_of_birth: '28',
+  departement_of_birth: '71',
   country_of_birth: 'France'
 )
-luke2 = Employee.create!(
-  firstname: 'Luke',
-  lastname: 'Skywalker',
-  company_id: company3.id,
-  address: '32 passage du trou, 25060 Montcuq',
+luc2 = Employee.create!(
+  firstname: 'Luc',
+  lastname: 'Mosebacke',
+  company_id: company1.id,
+  address: '32 passage de la coline, 25660 Montfaucon',
   phone: '0698798798',
   NIR: '555445554445544',
   conges_spectacles: '45645645',
   date_of_birth: Date.new(1962, 9, 12),
-  place_of_birth: 'Villedieu-Les-Poêles-sur-Saône',
-  departement_of_birth: '77',
+  place_of_birth: 'Villedieu',
+  departement_of_birth: '84',
   country_of_birth: 'France'
 )
 albert2 = Employee.create!(
   firstname: 'Albert',
-  lastname: 'de la Grenouillères des Andes',
-  company_id: company3.id,
+  lastname: 'Berthet',
+  company_id: company2.id,
   address: '366 place des monuments aux vétérans, 55800 Argenton-le-Pont',
   phone: '0685789445',
   NIR: '225413584684585',
   conges_spectacles: '12312531',
   date_of_birth: Date.new(2000, 1, 2),
   place_of_birth: 'Châlon-sur-Saône',
-  departement_of_birth: '28',
+  departement_of_birth: '71',
   country_of_birth: 'France'
 )
 
@@ -360,9 +360,9 @@ puts 'creating payslips'
 
 payslips = {
   payslip_number: 1,
-  employee_id: [jean_mich.id, fabrice.id, mamadou.id, luke.id, albert.id, luke2.id,albert2.id],
-  artists: [jean_mich.id, albert.id, luke2.id,albert2.id],
-  nartists: [fabrice.id, mamadou.id, luke.id],
+  employee_id: [jean.id, fabrice.id, mamadou.id, luc.id, albert.id, luc2.id,albert2.id],
+  artists: [jean.id, albert.id, luc2.id,albert2.id],
+  nartists: [fabrice.id, mamadou.id, luc.id],
   company_id: [company1.id, company2.id, company3.id, company1.id, company2.id, company3.id, company3.id],
   performance_id: [p1.id, p2.id, p3.id, p1.id, p2.id, p3.id, p3.id]
 }
