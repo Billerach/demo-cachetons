efc89f8de66df3d2d3b21e4c0b326ab929c5a0a6
Pascal Billerach <p.billerach@gmail.com>
2024-08-29 16:54:49 +0200

    Contributions seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -127,8 +127,8 @@ luc = Employee.create!(
   departement_of_birth: '84',
   country_of_birth: 'France'
 )
-albert = Employee.create!(
-  firstname: 'Albert',
+aline = Employee.create!(
+  firstname: 'Aline',
   lastname: 'Berthet',
   company_id: company2.id,
   address: '366 place des monuments aux vétérans, 55800 Argenton-le-Pont',
@@ -153,8 +153,8 @@ luc2 = Employee.create!(
   departement_of_birth: '84',
   country_of_birth: 'France'
 )
-albert2 = Employee.create!(
-  firstname: 'Albert',
+aline2 = Employee.create!(
+  firstname: 'Aline',
   lastname: 'Berthet',
   company_id: company2.id,
   address: '366 place des monuments aux vétérans, 55800 Argenton-le-Pont',
@@ -171,120 +171,14 @@ puts 'Employees created'
 
 puts 'Creating contributions'
 
-used_contributions = {
-  fund: [
-    'URSSAF',
-    'URSSAF',
-    'URSSAF',
-    'URSSAF',
-    'Assedic',
-    'URSSAF',
-    'URSSAF',
-    'URSSAF',
-    'Audiens',
-    'Audiens',
-    'Audiens',
-    'Audiens',
-    'CMB',
-    'URSSAF',
-    'URSSAF',
-    'FNAS',
-    'FCAP'
-  ],
-  name: [
-    '100 RG Base déplafonnée :  maladie, maternité, invalidité, décès, solidarité.',
-    '100D RG Base déplafonnée : allocations familiales.',
-    '100P RG Base plafonnée (vieillesse).',
-    'CSG déductible.',
-    "104 Assur. Chômage + 125 CDD contrat d'usage de - de 3 mois.",
-    '312D Artistes : vieillesse.',
-    '312A Artistes Accident du travail (code risque : 923AC).',
-    '334P FNAL Artistes et médecins - de 20 salariés.',
-    'Retraite ARRCO artiste non cadre Tranche A.',
-    'Retraite ARRCO non artiste non cadre Tranche A.',
-    'Prévoyance Artiste NC T1.',
-    'Prévoyance NC T1.',
-    'Médecine du travail (+ TVA à 20%).',
-    '671P Réduction Fillon Base plafonnée.',
-    '027D Contribution au dialogue social',
-    "Fond National d'Activités Sociales des entreprises artistiques et culturelles.",
-    "Fond Commun d'Aide au Paritarisme"
-  ],
-  employee_part: [0, 0, 0.069, 0.068, 0.0335, 0.0028, 0, 0, 0.0437, 0.0387, 0, 0, 0, 0, 0, 0, 0],
-  employer_part: [0.133, 0.03450, 0.08550, 0, 0.09551, 0.01331, 0.01191, 0.00071, 0.04380, 0.03880,
-                  0.00420, 0.00420, 0.00381, 0.28090, 0.0001, 0.01251, 0.00251],
+temp = Contribution.create!(
+  name: "312D Artistes : maladie, maternité, invalidité, décès, solidarité.",
+  fund: "URSSAF",
+  employee_part:0,
+  employer_part:0.094,
   start_date: '2023-01-01',
-  end_date: '2023-12-31'
-}
-
-used_contributions_array = []
-used_contributions[:fund].each_with_index do |fund, index|
-  used_contributions_array << Contribution.create!(
-    fund:,
-    name: used_contributions[:name][index],
-    employee_part: used_contributions[:employee_part][index],
-    employer_part: used_contributions[:employer_part][index],
-    start_date: used_contributions[:start_date],
-    end_date: used_contributions[:end_date]
+  end_date:  '2999-12-31'
   )
-end
-
-unused_contributions = {
-  fund: [
-    'URSSAF',
-    'URSSAF',
-    'URSSAF',
-    'URSSAF',
-    'URSSAF',
-    'URSSAF',
-    'URSSAF',
-    'Assedic',
-    'Audiens',
-    'Audiens',
-    'Audiens',
-    'CS',
-    'AFDAS',
-    'Audiens',
-    'URSSAF',
-    'AUDIENS',
-    'AUDIENS'
-  ],
-  name: [
-    '100D RG Base déplafonnée : vieillesse',
-    '100A RG Accident du travail (code risque : 923AD).',
-    '332P FNAL cas général et sect. public - de 20 salariés.',
-    'CSG/CRDS imposable.',
-    '312D Artistes :  maladie, maternité, invalidité, décès, solidarité.',
-    '312D Artistes : allocations familiales.',
-    '312P Artistes vieillesse.',
-    '301 AGS.',
-    'Retraite ARRCO cadres Tranche A + APEC',
-    'AGFF Tranche A (ARRCO).',
-    'Prévoyance Artiste Cadre T1.',
-    'Congés Spectacles.',
-    'Congés Formation (+ TVA à 20%).',
-    'CET Tranche A.',
-    '100A RG Accident du travail (code risque : 923AD).',
-    'Garantie minimale de points (GMP)',
-    'Garantie minimale de points (GMP)'
-  ],
-  employee_part: [0.004, 0, 0, 0.029, 0, 0, 0.0483, 0, 0.03894, 0.008, 0, 0, 0, 0.0013, 0, 1.42, 1.52],
-  employer_part: [0.019, 0.017, 0.001, 0, 0.094, 0.02421, 0.05991, 0.00151, 0.0391, 0.012, 0.01951, 0.152,
-                  0.02521, 0.00221, 0.017, 2.32, 2.48],
-  start_date: '2023-01-01',
-  end_date: '2023-12-31'
-}
-
-unused_contributions[:fund].each_with_index do |fund, index|
-  Contribution.create!(
-    fund:,
-    name: used_contributions[:name][index],
-    employee_part: used_contributions[:employee_part][index],
-    employer_part: used_contributions[:employer_part][index],
-    start_date: used_contributions[:start_date],
-    end_date: used_contributions[:end_date]
-  )
-end
 
 puts 'Contributions created'
 
@@ -349,9 +243,7 @@ puts 'creating job profiles to contributions links'
 
 all_job_profiles = JobProfile.all
 all_job_profiles.each do |job_profile|
-  used_contributions_array.each do |contribution|
-    JobProfilesToContributionsLink.create!(job_profile:, contribution:)
-  end
+  JobProfilesToContributionsLink.create!(job_profile:, contribution: temp)
 end
 
 puts 'job profiles to contributions links created'
@@ -360,8 +252,8 @@ puts 'creating payslips'
 
 payslips = {
   payslip_number: 1,
-  employee_id: [jean.id, fabrice.id, mamadou.id, luc.id, albert.id, luc2.id,albert2.id],
-  artists: [jean.id, albert.id, luc2.id,albert2.id],
+  employee_id: [jean.id, fabrice.id, mamadou.id, luc.id, aline.id, luc2.id,aline2.id],
+  artists: [jean.id, aline.id, luc2.id,aline2.id],
   nartists: [fabrice.id, mamadou.id, luc.id],
   company_id: [company1.id, company2.id, company3.id, company1.id, company2.id, company3.id, company3.id],
   performance_id: [p1.id, p2.id, p3.id, p1.id, p2.id, p3.id, p3.id]
@@ -447,9 +339,7 @@ puts 'Creatings payslips to contributions links'
 
 all_payslips = Payslip.all
 all_payslips.each do |payslip|
-  used_contributions_array.each do |contribution|
-    PayslipsToContributionsLink.create!(payslip:, contribution:)
-  end
+  PayslipsToContributionsLink.create!(payslip:, contribution: temp)
 end
 
 puts 'Payslips to contributions links created'
